d4aa1e2727fe9c2277736067390dd77b49e0352c
Vivian DELANNOY <vivian_dlny@hotmail.com>
2023-09-26 12:38:49 +0200

    🚚 rename app to aftermath

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Fuchsia
+# Aftermath
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fuchsia'`, () => {
+  it(`should have as title 'aftermath'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fuchsia');
+    expect(app.title).toEqual('aftermath');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fuchsia app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('aftermath app is running!');
   });
 });
